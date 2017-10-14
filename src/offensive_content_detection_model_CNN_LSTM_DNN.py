1b3f826146aaf3e190a1d654391d0b9f2ce55ba6
striker <aniruddha.ghosh@ucdconnect.ie>
2017-10-14 19:52:42 +0100

    model 1

--- src/offensive_content_detection_model_CNN_LSTM_DNN.py
+++ src/offensive_content_detection_model_CNN_LSTM_DNN.py
@@ -172,7 +172,7 @@ class train_model(offensive_content_model):
         dimension_size = 128
         W = None
         # W = dh.get_word2vec_weight(self._vocab, n=dimension_size, path='/home/word2vec/GoogleNews-vectors-negative300.bin')
-        W = dh.get_glove_weights(self._vocab, n=dimension_size, path='/home/TCDteam12/glove/glove_model.txt')
+        W = dh.get_glove_weights(self._vocab, n=200, path='/home/TCDteam12/glove/glove_model.txt')
         print('Word2vec obtained....')
 
         # solving class imbalance
