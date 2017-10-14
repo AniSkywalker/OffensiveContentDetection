40b4f232fa6705ddef0cb2f9575d2a8671ba5167
striker <aniruddha.ghosh@ucdconnect.ie>
2017-10-14 12:39:57 +0100

    model 1

--- src/offensive_content_detection_model_CNN_LSTM_DNN.py
+++ src/offensive_content_detection_model_CNN_LSTM_DNN.py
@@ -52,12 +52,12 @@ class offensive_content_model():
         print(model.output_shape)
 
         model.add(Convolution2D(hidden_units, (3,5), kernel_initializer='he_normal', padding='valid', activation='relu'))
-        # model.add(MaxPooling2D(2,2))
+        model.add(MaxPooling2D(2,2))
         model.add(Dropout(0.5))
 
-        # model.add(Convolution2D(hidden_units, (3,5), kernel_initializer='he_normal', padding='valid', activation='relu'))
-        # model.add(MaxPooling2D(2, 2))
-        # model.add(Dropout(0.5))
+        model.add(Convolution2D(hidden_units, (3,5), kernel_initializer='he_normal', padding='valid', activation='relu'))
+        model.add(MaxPooling2D(2, 2))
+        model.add(Dropout(0.5))
 
         # model.add(LSTM(hidden_units, kernel_initializer='he_normal', activation='sigmoid', return_sequences=True))
         # model.add(Dropout(0.25))
