994645b25f1be8bcb1c6e007a8b00a2d6f757a32
Martin McLaughlin <martinmclaughlin42@outlook.com>
2017-10-14 13:03:16 +0100

    added all_speech_format file

--- resource/train/process.py
+++ resource/train/process.py
@@ -6,11 +6,7 @@ def convert_one_line(text):
     return ' '.join([t.strip() for t in token])
 
 with open('hate_speech.train', newline='') as csvfile:
-    with open('hate_speech_format.train', 'w') as hate_writer:
-        with open('offensive_speech_format.train', 'w') as offensive_writer:
-            hate_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-            for row in hate_reader:
-                if row[5] == "0":
-                    hate_writer.write('ID'+'\t' +  row[5] + '\t' + convert_one_line(row[6]) + '\n')
-                elif row[5] == "1":
-                    offensive_writer.write('ID' +'\t' + row[5] + '\t' + convert_one_line(row[6]) + '\n')
+    with open('all_speech_format.train', 'w') as speech_writer:
+        hate_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in hate_reader:
+            speech_writer.write('ID'+'\t' +  row[5] + '\t' + convert_one_line(row[6]) + '\n')
