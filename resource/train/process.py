7bfff8fa1e44608376463ea179a94a7db404c4e9
Martin McLaughlin <martinmclaughlin42@outlook.com>
2017-10-14 13:50:41 +0100

    made new training data based on whiteboard rules

--- resource/train/process.py
+++ resource/train/process.py
@@ -6,7 +6,13 @@ def convert_one_line(text):
     return ' '.join([t.strip() for t in token])
 
 with open('hate_speech.train', newline='') as csvfile:
-    with open('all_speech_format.train', 'w') as speech_writer:
+    with open('neither_hate_speech_format.train', 'w') as speech_writer:
         hate_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         for row in hate_reader:
-            speech_writer.write('ID'+'\t' +  row[5] + '\t' + convert_one_line(row[6]) + '\n')
+            if row[5] == "0":
+                speech_writer.write('ID'+'\t' +  '1' + '\t' + convert_one_line(row[6]) + '\n')
+            elif row[5] == "1":
+                pass
+            elif row[5] == "2":
+                speech_writer.write('ID'+'\t' +  '0' + '\t' + convert_one_line(row[6]) + '\n')
+
