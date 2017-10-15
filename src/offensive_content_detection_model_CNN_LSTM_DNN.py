27d61fc7461a76998ef9ed9a1c1de47da7629abe
striker <aniruddha.ghosh@ucdconnect.ie>
2017-10-15 11:01:53 +0100

    new data

--- src/offensive_content_detection_model_CNN_LSTM_DNN.py
+++ src/offensive_content_detection_model_CNN_LSTM_DNN.py
@@ -347,30 +347,30 @@ if __name__ == "__main__":
 
     #hate_speech
 
-    train_file = basepath + '/resource/train/hate_speech_train.txt'
-    validation_file = basepath + '/resource/test/train_english.txt.test'
-    test_file = basepath + '/resource/dev/train_english.txt.train'
-    word_file_path = basepath + '/resource/word_list.txt'
-
-    output_file = basepath + '/resource/text_model/TestResults_hate.txt'
-    model_file = basepath + '/resource/text_model/weights/'
-    vocab_file_path = basepath + '/resource/text_model/vocab_list.txt'
-
-    tr = train_model(train_file, train_file, word_file_path, model_file, vocab_file_path, output_file,model_filename='hate_speech.json')
-
-    # emotion
-
-    # train_file = basepath + '/resource/train/emotion_train_v3.txt'
-    # validation_file = basepath + '/resource/test/emotion_train.txt'
+    # train_file = basepath + '/resource/train/hate_speech_train.txt'
+    # validation_file = basepath + '/resource/test/train_english.txt.test'
     # test_file = basepath + '/resource/dev/train_english.txt.train'
     # word_file_path = basepath + '/resource/word_list.txt'
     #
-    # output_file = basepath + '/resource/text_model/TestResults_emotion.txt'
+    # output_file = basepath + '/resource/text_model/TestResults_hate.txt'
     # model_file = basepath + '/resource/text_model/weights/'
     # vocab_file_path = basepath + '/resource/text_model/vocab_list.txt'
 
-    # tr = train_model(train_file, train_file, word_file_path, model_file, vocab_file_path, output_file,
-    #                  model_filename='emotion.json')
+    # tr = train_model(train_file, train_file, word_file_path, model_file, vocab_file_path, output_file,model_filename='hate_speech.json')
+
+    # emotion
+
+    train_file = basepath + '/resource/train/emotion_train_v3.txt'
+    validation_file = basepath + '/resource/test/emotion_train.txt'
+    test_file = basepath + '/resource/dev/train_english.txt.train'
+    word_file_path = basepath + '/resource/word_list.txt'
+
+    output_file = basepath + '/resource/text_model/TestResults_emotion.txt'
+    model_file = basepath + '/resource/text_model/weights/'
+    vocab_file_path = basepath + '/resource/text_model/vocab_list.txt'
+
+    tr = train_model(train_file, train_file, word_file_path, model_file, vocab_file_path, output_file,
+                     model_filename='emotion.json')
 
 
 
